c7f2de78068a4be9756148b567b3f890dc8948a0
coflskim <qawse980@gmail.com>
2024-01-19 16:19:36 +0900

    [feat] 예시

--- src/main/java/klieme/artdiary/ArtdiaryApplication.java
+++ src/main/java/klieme/artdiary/ArtdiaryApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ArtdiaryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ArtdiaryApplication.class, args);
 	}
