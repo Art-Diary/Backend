693423f571d1eb97f01c87bddaf2ef949db62a15
coflskim <qawse980@gmail.com>
2024-01-29 15:59:22 +0900

    [feature] 유저 더미 데이터 생성

--- src/main/java/klieme/artdiary/users/service/UserService.java
+++ src/main/java/klieme/artdiary/users/service/UserService.java
@@ -1,7 +1,35 @@
 package klieme.artdiary.users.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import klieme.artdiary.users.data_access.entity.UserEntity;
+import klieme.artdiary.users.data_access.repository.UserRepository;
+
 @Service
-public class UserService {
+public class UserService implements UserOperationUseCase {
+
+	private final UserRepository userRepository;
+
+	@Autowired
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public String createDummy(UserDummyCreateCommand command) {
+		UserEntity entity = UserEntity.builder()
+			.email(command.getEmail())
+			.nickname(command.getNickname())
+			.profile(command.getProfile())
+			.providerType(command.getProviderType())
+			.providerId(command.getProviderId())
+			.favoriteArt(command.getFavoriteArt())
+			.alarm1(command.getAlarm1())
+			.alarm2(command.getAlarm2())
+			.alarm3(command.getAlarm3())
+			.build();
+		userRepository.save(entity);
+		return "complete";
+	}
 }
--- src/main/java/klieme/artdiary/users/ui/controller/UserController.java
+++ src/main/java/klieme/artdiary/users/ui/controller/UserController.java
@@ -1,12 +1,45 @@
 package klieme.artdiary.users.ui.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import klieme.artdiary.users.service.UserOperationUseCase;
+import klieme.artdiary.users.ui.request_body.UserRequest;
 
 @RestController
 public class UserController {
+
+	private final UserOperationUseCase userOperationUseCase;
+
+	@Autowired
+	public UserController(UserOperationUseCase userOperationUseCase) {
+		this.userOperationUseCase = userOperationUseCase;
+	}
+
 	@GetMapping("/hello")
 	public void helloPrint() {
 		System.out.println("hello");
+	}
+
+	@PostMapping("")
+	public void createDummyData(@Valid @RequestBody UserRequest userRequest) {
+		System.out.println("test");
+
+		var command = UserOperationUseCase.UserDummyCreateCommand.builder()
+			.email(userRequest.getEmail())
+			.nickname(userRequest.getNickname())
+			.profile(userRequest.getProfile())
+			.providerType(userRequest.getProviderType())
+			.providerId(userRequest.getProviderId())
+			.favoriteArt(userRequest.getFavoriteArt())
+			.alarm1(userRequest.getAlarm1())
+			.alarm2(userRequest.getAlarm2())
+			.alarm3(userRequest.getAlarm3())
+			.build();
+		System.out.println(userOperationUseCase.createDummy(command));
 	}
 }
