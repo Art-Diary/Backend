11a2ded63d4a9e8f06f35a757999e740da813179
hiwon2 <leehw1997@gmail.com>
2024-02-07 14:51:04 +0900

    [feature] mydiarys-기본-파일-생성 table이름 수정

--- src/main/java/klieme/artdiary/mydiarys/data_access/entity/MydiaryEntity.java
+++ src/main/java/klieme/artdiary/mydiarys/data_access/entity/MydiaryEntity.java
@@ -17,7 +17,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "diary")
+@Table(name = "solo_diary")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @DynamicInsert
