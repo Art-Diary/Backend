0f66976aa64c582a07b3a176bbe9e6105cd74a25
cofls980 <qawse980@gmail.com>
2024-02-16 00:01:54 +0900

    [feature] 전시 메이트가 갔다온 전시회 목록 조회 구현 (전시 메이트 검증 제외)

---
+++ src/main/java/klieme/artdiary/mateexhs/service/MateExhsService.java
@@ -0,0 +1,105 @@
+package klieme.artdiary.mateexhs.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import klieme.artdiary.common.UserIdFilter;
+import klieme.artdiary.exhibitions.data_access.entity.ExhEntity;
+import klieme.artdiary.exhibitions.data_access.entity.UserExhEntity;
+import klieme.artdiary.exhibitions.data_access.repository.ExhRepository;
+import klieme.artdiary.exhibitions.data_access.repository.UserExhRepository;
+import klieme.artdiary.gatherings.data_access.entity.GatheringDiaryEntity;
+import klieme.artdiary.gatherings.data_access.entity.GatheringExhEntity;
+import klieme.artdiary.gatherings.data_access.repository.GatheringDiaryRepository;
+import klieme.artdiary.gatherings.data_access.repository.GatheringExhRepository;
+import klieme.artdiary.mydiarys.data_access.entity.MydiaryEntity;
+import klieme.artdiary.mydiarys.data_access.repository.MydiaryRepository;
+
+@Service
+public class MateExhsService implements MateExhsReadUseCase {
+	private final UserExhRepository userExhRepository;
+	private final MydiaryRepository mydiaryRepository;
+	private final GatheringDiaryRepository gatheringDiaryRepository;
+	private final GatheringExhRepository gatheringExhRepository;
+	private final ExhRepository exhRepository;
+
+	@Autowired
+	public MateExhsService(UserExhRepository userExhRepository, MydiaryRepository mydiaryRepository,
+		GatheringDiaryRepository gatheringDiaryRepository, GatheringExhRepository gatheringExhRepository,
+		ExhRepository exhRepository) {
+		this.userExhRepository = userExhRepository;
+		this.mydiaryRepository = mydiaryRepository;
+		this.gatheringDiaryRepository = gatheringDiaryRepository;
+		this.gatheringExhRepository = gatheringExhRepository;
+		this.exhRepository = exhRepository;
+	}
+
+	@Override
+	public List<FindMateExhsResult> getMateExhsList(MateExhsFindQuery query) {
+		// 내 친구가 맞는지 확인 - exh_mate 확인
+		// TODO exh_mate 생성 후 구현
+		// 친구의 기록이 있는 전시회 조회
+		List<Long> checkExhs = new ArrayList<>(); // 같은 전시회지만 방문 날짜가 다른 경우의 중복을 없애기 위해 사용
+		HashMap<Long, Integer> countDiary = new HashMap<>(); // 전시회에 대한 기록 개수
+		HashMap<Long, Double> sumDiaryRate = new HashMap<>(); // 전시회에 대한 기록 별점 합
+		// 1. userExh에서 친구가 작성한 기록이 있는 경우
+		List<UserExhEntity> mateExhEntities = userExhRepository.findByUserId(query.getMateId());
+		for (UserExhEntity mateExhEntity : mateExhEntities) {
+			List<MydiaryEntity> mateDiaryEntities = mydiaryRepository.findByUserExhId(mateExhEntity.getUserExhId());
+
+			countDiary.putIfAbsent(mateExhEntity.getExhId(), 0);
+			sumDiaryRate.putIfAbsent(mateExhEntity.getExhId(), 0.0);
+			for (MydiaryEntity mateDiaryEntity : mateDiaryEntities) {
+				Integer countExh = countDiary.get(mateExhEntity.getExhId());
+				Double sumExhRate = sumDiaryRate.get(mateExhEntity.getExhId());
+				countDiary.put(mateExhEntity.getExhId(), countExh + 1);
+				sumDiaryRate.put(mateExhEntity.getExhId(), sumExhRate + mateDiaryEntity.getRate());
+			}
+			// 전시회 중복 제거
+			if (!checkExhs.contains(mateExhEntity.getExhId())) {
+				checkExhs.add(mateExhEntity.getExhId());
+			}
+		}
+		// 2. gatheringDiary애서 userId를 통해 친구가 작성한 기록이 있는 경우
+		List<GatheringDiaryEntity> gatheringDiaryEntities = gatheringDiaryRepository.findByUserId(query.getMateId());
+		for (GatheringDiaryEntity gatheringDiary : gatheringDiaryEntities) {
+			Optional<GatheringExhEntity> gatheringExhEntity = gatheringExhRepository.findByGatheringExhId(
+				gatheringDiary.getGatheringExhId());
+			if (gatheringExhEntity.isEmpty()) {
+				continue;
+			}
+			int countExh = countDiary.get(gatheringExhEntity.get().getExhId()) == null ? 0 :
+				countDiary.get(gatheringExhEntity.get().getExhId());
+			Double sumExhRate = sumDiaryRate.get(gatheringExhEntity.get().getExhId()) == null ? 0.0 :
+				sumDiaryRate.get(gatheringExhEntity.get().getExhId());
+			countDiary.put(gatheringExhEntity.get().getExhId(), countExh + 1);
+			sumDiaryRate.put(gatheringExhEntity.get().getExhId(), sumExhRate + gatheringDiary.getRate());
+			// 전시회 중복 제거
+			if (!checkExhs.contains(gatheringExhEntity.get().getExhId())) {
+				checkExhs.add(gatheringExhEntity.get().getExhId());
+			}
+		}
+		// 반환
+		List<FindMateExhsResult> result = new ArrayList<>();
+		for (Long exhId : checkExhs) {
+			Optional<ExhEntity> exh = exhRepository.findByExhId(exhId);
+			/* TODO
+			 * 친구가 작성한 글들의 평점?으로 구현함.
+			 * 저장된 포스터 사진 있으면 구현
+			 * 아래 코드의 null 수정 필요
+			 */
+			Double averageRate = countDiary.get(exhId) == 0 ? 0.0 : sumDiaryRate.get(exhId) / countDiary.get(exhId);
+			exh.ifPresent(exhEntity -> result.add(FindMateExhsResult.findMateExhs(exhEntity, null, averageRate)));
+		}
+		return result;
+	}
+
+	private Long getUserId() {
+		return UserIdFilter.getUserId();
+	}
+}
---
+++ src/main/java/klieme/artdiary/mateexhs/ui/controller/MateExhsController.java
@@ -0,0 +1,39 @@
+package klieme.artdiary.mateexhs.ui.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import klieme.artdiary.mateexhs.service.MateExhsReadUseCase;
+import klieme.artdiary.mateexhs.ui.view.MateExhsView;
+
+@RestController
+@RequestMapping(value = "/mates/{mateId}/exhibitions")
+public class MateExhsController {
+	private final MateExhsReadUseCase mateExhsReadUseCase;
+
+	@Autowired
+	public MateExhsController(MateExhsReadUseCase mateExhsReadUseCase) {
+		this.mateExhsReadUseCase = mateExhsReadUseCase;
+	}
+
+	@GetMapping("")
+	public ResponseEntity<List<MateExhsView>> getMateExhsList(@PathVariable(name = "mateId") Long mateId) {
+		var query = MateExhsReadUseCase.MateExhsFindQuery.builder().mateId(mateId).build();
+		// 비즈니스 로직 호출
+		List<MateExhsReadUseCase.FindMateExhsResult> results = mateExhsReadUseCase.getMateExhsList(query);
+		// 비즈니스 로직 결과값을 view 형식에 맞춰 list로 반환
+		List<MateExhsView> viewResult = new ArrayList<>();
+
+		for (MateExhsReadUseCase.FindMateExhsResult result : results) {
+			viewResult.add(MateExhsView.builder().result(result).build());
+		}
+		return ResponseEntity.ok(viewResult);
+	}
+}
