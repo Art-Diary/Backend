d7af25e5c0841752216b338e96f22d344aa1acbb
hiwon2 <leehw1997@gmail.com>
2024-02-19 17:24:20 +0900

    [feature] 전시메이트 추가

--- src/main/java/klieme/artdiary/mate/service/MateService.java
+++ src/main/java/klieme/artdiary/mate/service/MateService.java
@@ -2,19 +2,25 @@ package klieme.artdiary.mate.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
+import klieme.artdiary.common.ArtDiaryException;
+import klieme.artdiary.common.MessageType;
 import klieme.artdiary.common.UserIdFilter;
+import klieme.artdiary.exhibitions.data_access.entity.UserExhEntity;
+import klieme.artdiary.gatherings.data_access.entity.GatheringMateId;
 import klieme.artdiary.mate.data_access.entity.MateEntity;
 import klieme.artdiary.mate.data_access.repository.MateRepository;
 import klieme.artdiary.users.data_access.entity.UserEntity;
 import klieme.artdiary.users.data_access.repository.UserRepository;
 
 @Service
-public class MateService implements MateReadUseCase {
+public class MateService implements MateReadUseCase, MateOperationUseCase {
 	private final MateRepository mateRepository;
 	private final UserRepository userRepository;
 
@@ -39,6 +45,50 @@ public class MateService implements MateReadUseCase {
 			userEntity.ifPresent(user -> results.add(MateReadUseCase.FindMateResult.findByGatheringExhs(user, null)));
 		}
 		return results;
+	}
+
+	@Override
+	@Transactional
+	public List<MateReadUseCase.FindMateResult> addMyMateCreate(MateOperationUseCase.AddMyMateCreateDummy dummy) {
+
+		//user에 있는지 확인
+		UserEntity checkEntity = userRepository.findByUserId(dummy.getToUserId())
+			.orElseThrow(() -> new ArtDiaryException(MessageType.NOT_FOUND));
+
+		//나인지 확인
+		if (checkEntity.getUserId().equals(dummy.getToUserId())) {
+			throw new ArtDiaryException(MessageType.FORBIDDEN);
+		}
+		//exh_mate에서 이미 저장한 친구인지 확인
+		Optional<MateEntity> entity = mateRepository.findByFromUserIdAndToUserId(getUserId(), dummy.getToUserId());
+
+		if (entity.isPresent()) {
+			throw new ArtDiaryException(MessageType.CONFLICT);
+		}
+		//exh_mate에 저장
+		MateEntity newMate = MateEntity.builder()
+			.toUserId(dummy.getToUserId())
+			.fromUserId(getUserId())
+			.build();
+
+		mateRepository.save(newMate);
+
+		List<MateEntity> allMateEntities = mateRepository.findByFromUserId(getUserId());
+		List<MateReadUseCase.FindMateResult> results = new ArrayList<>();
+		for (MateEntity allMateEntity : allMateEntities) {
+			UserEntity tmp = userRepository.findByUserId(allMateEntity.getToUserId())
+				.orElseThrow(() -> new ArtDiaryException(MessageType.NOT_FOUND));
+			MateReadUseCase.FindMateResult result = MateReadUseCase.FindMateResult.builder()
+				.userId(tmp.getUserId())
+				.nickname(tmp.getNickname())
+				.profile(tmp.getProfile())
+				.favoriteArt(tmp.getFavoriteArt())
+				.build();
+			results.add(result);
+		}
+
+		return results;
+
 	}
 
 	private Long getUserId() {
